149704672390a58cdcf3513e7b46f5885e36343f
Sagar <56674688+sagar-tan@users.noreply.github.com>
2025-03-05 11:08:17 +0530

    Time: 1 ms (30.25%), Space: 7.8 MB (61.42%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    long long coloredCells(int n) {
+    long long coloredCells(int n) {//using For Loop
         long ans = 1;
         long sum = 0;
         for(int i=1;i<n+1;i++){
             sum+=ans;
             ans=i*4;
-            
         }
         return sum;
     }
-        
 };
