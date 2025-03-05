9c2ab73071fd9a250b0ab3a36eec6014a899475a
Sagar <56674688+sagar-tan@users.noreply.github.com>
2025-03-05 10:23:30 +0530

    Time: 0 ms (100%), Space: 7.8 MB (91.36%) - LeetHub

---
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        long ans = 1;
+        long sum = 0;
+        for(int i=1;i<n+1;i++){
+            sum+=ans;
+            ans=i*4;
+            
+        }
+        return sum;
+    }
+        
+};
