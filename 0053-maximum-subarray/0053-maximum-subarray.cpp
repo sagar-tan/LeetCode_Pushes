f251d7692add214b67ecb449903737496cf42aeb
Sagar <56674688+sagar-tan@users.noreply.github.com>
2025-03-06 11:41:08 +0530

    Time: 0 ms (100%), Space: 74.5 MB (5.22%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int n  = nums.size();
+    int maxSubArray(vector<int>& nums) {//Unoptimised Using 2 Loops
+        int n  = nums.size(); 
         vector<int> pref(n);
         pref[0] = nums[0];
         for(int i = 1; i<n;i++){
