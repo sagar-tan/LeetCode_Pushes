162862097ec300b4316076b18b61f2e8f31fe2d9
Sagar <56674688+sagar-tan@users.noreply.github.com>
2025-03-14 10:41:18 +0530

    Time: 6 ms (98.81%), Space: 88.3 MB (41.19%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -1,49 +1,21 @@
 class Solution {
-public:
-    int maximumCandies(vector<int>& candies, long long k) {
-        // Find the maximum number of candies in any pile
-        int maxCandiesInPile = 0;
-        for (int i = 0; i < candies.size(); i++) {
-            maxCandiesInPile = max(maxCandiesInPile, candies[i]);
+    bool isPossible(vector<int>& can, long long mid, long long k) {
+        long long total = 0;
+        for(auto x: can) {
+            total += x / mid;
         }
-
-        // Set the initial search range for binary search
-        int left = 0;
-        int right = maxCandiesInPile;
-
-        // Binary search to find the maximum number of candies each child can
-        // get
-        while (left < right) {
-            // Calculate the middle value of the current range
-            int middle = (left + right + 1) / 2;
-
-            // Check if it's possible to allocate candies so that each child
-            // gets 'middle' candies
-            if (canAllocateCandies(candies, k, middle)) {
-                // If possible, move to the upper half to search for a larger
-                // number
-                left = middle;
-            } else {
-                // Otherwise, move to the lower half
-                right = middle - 1;
-            }
-        }
-
-        return left;
+        return total >= k;
     }
-
-private:
-    bool canAllocateCandies(vector<int>& candies, long long k,
-                            int numOfCandies) {
-        // Initialize the total number of children that can be served
-        long long int maxNumOfChildren = 0;
-
-        for (int pileIndex = 0; pileIndex < candies.size(); pileIndex++) {
-            maxNumOfChildren +=
-                candies[pileIndex] /
-                numOfCandies;  // Add the number of children this pile can serve
+public:
+    int maximumCandies(vector<int>& can, long long k) {
+        long long n = can.size(), end = 0, start = 1;
+        for(auto x: can) end += x;
+        end /= k;
+        while(start <= end) {
+            long long mid = start + (end - start) / 2;
+            if(isPossible(can, mid, k)) start = mid + 1;
+            else end = mid - 1;
         }
-
-        return maxNumOfChildren >= k;
+        return end;
     }
 };
